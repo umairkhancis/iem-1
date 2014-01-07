1084cd85048c19a94bd07b1df85e97a80d35ca2c
Ahmed Nasir <ahmednasir@live.com>
2014-01-07 12:11:41 +0500

    fixed user login

---
+++ src/com/netpace/iem/ahmed/controllers/LoginController.java
@@ -0,0 +1,21 @@
+package com.netpace.iem.ahmed.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String login(ModelMap model) { 
+		return "login"; 
+	}
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logout(ModelMap model) { 
+		return "login"; 
+	}
+	
+}
